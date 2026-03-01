3147699becb75913175c31f9bb8085c8afc90f7a
titan1975 <efstathios.arethas@gmail.com>
2026-03-01 10:24:41 -0500

    Add Athena chat widget to all pages

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
     <Navbar />
     <router-view />
     <Footer />
+    <AthenaBot />
   </div>
 </template>
 
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+import AthenaBot from './components/AthenaBot.vue'
 </script>
 
 <style>
